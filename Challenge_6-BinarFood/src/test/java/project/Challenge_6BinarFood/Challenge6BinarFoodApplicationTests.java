f65e081b16abd7b2aaa41d9372568aef4215efea
fajartu <twentyone1998@gmail.com>
2024-02-26 21:17:51 +0700

    update spring boot test

--- Challenge_6-BinarFood/src/test/java/project/Challenge_6BinarFood/Challenge6BinarFoodApplicationTests.java
+++ Challenge_6-BinarFood/src/test/java/project/Challenge_6BinarFood/Challenge6BinarFoodApplicationTests.java
@@ -2,8 +2,9 @@ package project.Challenge_6BinarFood;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-@SpringBootTest
+@SpringJUnitConfig
 class Challenge6BinarFoodApplicationTests {
 
 	@Test
